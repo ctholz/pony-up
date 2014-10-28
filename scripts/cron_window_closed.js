cd366f9ca86328c58a86140b536cd2da7346d7e9
@holz <ctholz@gmail.com>
2014-10-28 10:18:02 -0700

    Jimmying pick carryover script to make old picks

--- scripts/cron_window_closed.js
+++ scripts/cron_window_closed.js
@@ -46,20 +46,24 @@ var initializer = require('../initializer');
 
 // Input: user
 	// Output: checks if user has picks for this week. If not, copies over from last week
-function processUsersPicks(user, callback) {
-	db.Pick.find({where:{ UserId: user.id, week: CONSTANTS.WEEK_OF_SEASON }}).success(function(pick) {
+function processUsersPicks(user, week, callback) {
+	db.Pick.find({where:{ UserId: user.id, week: week }}).success(function(pick) {
 		if (pick) {
 			console.log("[",user.email,"] has picked, returning.");
 			return callback();
 		} else {
 
 			// Get last week's picks
-			db.Pick.find({where:{ UserId: user.id, week: CONSTANTS.WEEK_OF_SEASON - 1}}).success(function(last_week_pick) {
+			db.Pick.find({where:{ UserId: user.id, week: week - 1}}).success(function(last_week_pick) {
+
+				if (!last_week_pick)
+					return callback("["+user.email+"] No picks for last week - " + (week - 1) + " - found.")
+				
 				// Create this week's picks
 				db.Pick.create({
 					picks: 		last_week_pick.picks,
 					status: 	'active',
-					week: 		CONSTANTS.WEEK_OF_SEASON,
+					week: 		week,
 					UserId: 	user.id,
 				}).success(function(pick_set) {
 					console.log("COPYING PICKS FOR [",user.email,"] - Done.");
@@ -72,16 +76,16 @@ function processUsersPicks(user, callback) {
 	});
 };
 
-function main() {
+function main(week) {
 
 	db.User.findAll({where:{status:'active'}}).success(function(users) {
 		var count = users.length;
 
 		users.forEach(function(user) {
 
-			processUsersPicks(user, function(err) {
+			processUsersPicks(user, week, function(err) {
 				if (err)
-					console.log("ERROR - ",err);
+					console.log("	ERROR - ",err);
 
 				if (--count == 0) {
 					console.log("[---Done---]\n");
@@ -103,12 +107,17 @@ db.sequelize.sync().complete(function(err) {
         // set dev or production env variable
         global.dev = (app.get('env') == 'development' && !('HEROKU_POSTGRESQL_GOLD_URL' in process.env)) ? true : false;  
     
-   		// Only run on Tuesday
-        var d = new Date();
-        if (d.getDay() != 4) {
-        	console.log('CRON - window_closed - not Thursday, sleepy time.')
-        	process.exit()
-        } else
-   			main();
+        // 
+        var WEEK_OVERRIDE = (process.argv.length > 2) ? parseInt(process.argv[2]) : CONSTANTS.WEEK_OF_SEASON;
+        main(WEEK_OVERRIDE)
+   		// // Only run on Tuesday
+     //    var d = new Date();
+     //    if (d.getDay() != 4) {
+     //    	console.log('CRON - window_closed - not Thursday, sleepy time.')
+     //    	process.exit()
+     //    } else
+   		// 	main(WEEK_OVERRIDE);
+
+
     });
 });
